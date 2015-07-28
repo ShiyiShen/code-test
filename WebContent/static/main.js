d37672aac2c074158f7f34a356ffde14231e0bae
Shiyi Shen <shen.shiyi2007@gmail.com>
2015-07-28 16:01:44 -0400

    add slider image with text

--- WebContent/static/main.js
+++ WebContent/static/main.js
@@ -29,5 +29,24 @@ $(document).ready(function(){
 		
 	});	
 	
+	
+	/*==================== front Page Slider =====================*/
+	
+	  $("#front-page-slide-owl").owlCarousel({			  
+	      slideSpeed : 20,
+	      paginationSpeed : 400,
+	      singleItem:true,
+	      autoPlay: true,
+	      stopOnHover: true,
+	      lazyLoad: true,
+	      afterInit  : function callBack() {	        	  
+	          var content =  $(".item .info-content");
+	          content.delay(300).animate({"top": "15%", opacity: 1}, "slow");		
+	      },
+	      afterMove : function callBack() {	        	  
+	          var content =  $(".item .info-content");
+	          content.delay(300).animate({"top": "15%", opacity: 1}, "slow");		
+	      }
+	  });
 
 });
