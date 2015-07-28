00749a74303557c6aaa67d5c44941785deb25536
Shiyi Shen <shen.shiyi2007@gmail.com>
2015-07-28 12:41:45 -0400

    sticky and responsive header/menu

--- WebContent/static/main.js
+++ WebContent/static/main.js
@@ -4,4 +4,30 @@ $(document).ready(function(){
 
 	})
 
+	
+	/* ==================== menu ===================== */				
+	$('#header .navbar-toggle').on('click', function(){
+		var menu = $("#header .menu-wrapper");
+			
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}		
+	});
+	
+	
+	$('#header .menu-wrapper').on('click', function(){
+		var menu = $('#header .menu-wrapper');		
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}
+		
+	});	
+	
+
 });
